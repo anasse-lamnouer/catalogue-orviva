f34e9a8805fc3e64c5444ff28c812552359c6ac3
anasse lamnouer <lamnoueranasse.email@gmail.com>
2026-07-16 13:07:41 +0100

    Description des modifications

--- mobilite/Scooters_électriques.js
+++ mobilite/Scooters_électriques.js
@@ -18,7 +18,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://whill.inc/us/wp-content/uploads/2025/06/Model-R3-product-1.webp",
-            prix: "256 693.00 MAD",
+            prix: "25 669.30 MAD",
             titre: "WHILL Model R 3-Wheel",
             lien: "https://whill.inc/us/whill-model-r-3-wheel/"
         }
@@ -40,7 +40,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://www.hammer-international.de/cdn/shop/files/R3_005_720x.jpg?v=1741873445",
-            prix: "427 893.00 MAD",
+            prix: "42 789.30 MAD",
             titre: "Whill Modèle R 3 roues 6 km/h",
             lien: "https://www.hammer-international.de/en/products/whill-model_r-3-rad-6-km-h"
         }
@@ -62,7 +62,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://www.seacroftmobility.com/cdn/shop/files/Pridegogocompact.jpg?v=1755599094",
-            prix: "240 054.07 MAD",
+            prix: "24 005.40 MAD",
             titre: "Scooter électrique intelligent Robooter Q40 pour seniors",
             lien: "https://www.hohomedical.com/products/robooter-q40"
         }
@@ -84,7 +84,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://kmina.com/cdn/shop/files/Scooter_Electrico_Blanco_Adultos_ligero_939x939.jpg?v=1776770446",
-            prix: "160 498.93 MAD",
+            prix: "16 049.89 MAD",
             titre: "Trottinette électrique pliable blanche pour adultes",
             lien: "https://kmina.com/en-int/products/scooter-electrico-plegable-adultos-blanco"
         }
@@ -106,7 +106,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://kmina.com/cdn/shop/files/ScooterElectricoTodoterrenoMayores_939x939.jpg?v=1753456600",
-            prix: "376 640.00 MAD",
+            prix: "37 664.00 MAD",
             titre: "Trottinette électrique tout-terrain pour adultes",
             lien: "https://kmina.com/en-int/products/scooter-electrico-todoterreno"
         }
@@ -128,7 +128,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://accesshub.at/wp-content/uploads/2025/05/ONE-25AH-Air-tires-3Q-view-1024x912.jpg.webp",
-            prix: "237 540.00 MAD",
+            prix: "23 754.00 MAD",
             titre: "ONE Air+ (Trottinette électrique intérieure et extérieure)",
             lien: "https://accesshub.at/en/produkte/one-air/"
         }
@@ -144,13 +144,13 @@ window.produitsData.push(
         },
         itemB: {
             img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2JRjHrXBBga4uWiPIYvqiNqdz2AP-S1tQcbwd47f10g&s=10",
-            prix: "0.00 MAD",
-            titre: "Déambulateur articulé pliable Noir",
-            lien: "https://locamed.ma/product/deambulateur-articule-pliable-noir/"
+            prix: "24 500.00 MAD",
+            titre: "Fauteuil roulant électrique scooter",
+            lien: "https://locamed.ma/product/fauteuil-roulant-electrique-scooter/"
         },
         itemC: {
             img: "https://bastide-bastide-storage.omn.proximis.com/Imagestorage/imagesSynchro/1520/1520/efc44ff3ec5d8d13d200d265c7ca0f8746de0ea0_scooter_colibri_pour_handicape_outdoor.jpg",
-            prix: "224 700.00 MAD",
+            prix: "22 470.00 MAD",
             titre: "Scooter Colibri bleu Invacare Outdoor",
             lien: "https://www.bastideleconfortmedical.com/scooter-colibri-bleu-invacare-outdoor-710318.html"
         }
@@ -172,7 +172,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://media.ortopediayrehabilitacion.com/product/scooter-eris-4-ruedas-800x800.jpg?width=1200",
-            prix: "255 944.00 MAD",
+            prix: "25 594.40 MAD",
             titre: "Scooter à 4 roues Eris",
             lien: "https://www.ortopediayrehabilitacion.com/scooter-eris-4-ruedas-b18d3/"
         }
@@ -194,7 +194,7 @@ window.produitsData.push(
         },
         itemC: {
             img: "https://kmina.com/cdn/shop/files/Principalalternativa_939x939.png?v=1776770545",
-            prix: "117 698.93 MAD",
+            prix: "11 769.89 MAD",
             titre: "Trottinette électrique pour adultes",
             lien: "https://kmina.com/en-int/products/scooter-electrico-para-adultos"
         }
